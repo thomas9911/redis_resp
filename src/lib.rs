8079350c3646dd6c472c9c54821a90efe0b2899c
Thomas Timmer <thomastimmer11@hotmail.com>
2023-02-05 13:28:26 +0100

    feat: add value helper type

--- src/lib.rs
+++ src/lib.rs
@@ -13,15 +13,27 @@ mod parser;
 pub use lexer::Lexer;
 pub use parser::Parser;
 
-type ResultXd = Result<RespType, RespError>;
-type ResultXp<'a> = Result<RespTypeScan<'a>, RespError>;
+#[derive(Debug, PartialEq, Clone)]
+pub enum Value {
+    Bytes(Vec<u8>),
+    String(String),
+    Int(i64),
+    Array(Vec<Value>),
+    Null,
+}
 
-#[derive(Debug, PartialEq)]
-pub enum RespError {
-    None,
-    Other,
-    NewLineMissing,
-    InvalidStart,
+impl Into<RespType> for Value {
+    fn into(self) -> RespType {
+        use Value::*;
+
+        match self {
+            Bytes(data) => RespType::BulkString(data),
+            String(data) => RespType::BulkString(data.into()),
+            Int(data) => RespType::Integer(data),
+            Array(data) => RespType::Array(data.into_iter().map(|x| x.into()).collect()),
+            Null => RespType::NullString,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -78,287 +90,181 @@ pub enum RespType {
 }
 
 impl RespType {
-    pub fn parse(input: &[u8]) -> ResultXd {
-        if input.len() == 0 {
-            return Err(RespError::Other);
-        }
+    pub fn is_null(&self) -> bool {
+        use RespType::*;
 
-        match input[0] {
-            b'+' => parse_simple_string(&input[1..]),
-            _ => return Err(RespError::Other),
+        match self {
+            NullString => true,
+            NullArray => true,
+            _ => false,
         }
     }
-}
 
-fn parse_simple_string(input: &[u8]) -> ResultXd {
-    let mut iter = input.iter().peekable();
-    let mut data = Vec::with_capacity(input.len());
-    let mut closed = false;
+    pub fn as_bytes(&self) -> Option<&[u8]> {
+        use RespType::*;
 
-    while let Some(a) = iter.next() {
-        if a == &b'\r' && iter.peek() == Some(&&b'\n') {
-            closed = true;
-            break;
+        match self {
+            SimpleString(data) => Some(data),
+            BulkString(data) => Some(data),
+            _ => None,
         }
-
-        data.push(*a)
     }
 
-    if closed {
-        Ok(RespType::SimpleString(data))
-    } else {
-        Err(RespError::Other)
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub enum RespTypeScan<'a> {
-    SimpleString(&'a [u8]),
-    Error(&'a [u8]),
-    Integer(i64),
-    BulkString(&'a [u8]),
-    NullString,
-    NullArray,
-    ArrayStart(usize),
-}
-
-impl<'a> RespTypeScan<'a> {
-    pub fn iter(data: &'a [u8]) -> RespTypeIter<'a> {
-        RespTypeIter::new(data)
-    }
-
-    fn get_line_range(input: &[u8], start_at: usize) -> Result<(usize, usize), RespError> {
-        let mut iter = input[start_at..].iter().peekable();
-        let mut closed = false;
-        let mut end = start_at;
-
-        while let Some(a) = iter.next() {
-            if a == &b'\r' && iter.peek() == Some(&&b'\n') {
-                closed = true;
-                end += 2;
-                break;
-            }
-
-            end += 1;
-        }
-
-        if closed {
-            Ok((start_at, end))
+    pub fn as_error_bytes(&self) -> Option<&[u8]> {
+        if let RespType::Error(error) = self {
+            Some(error)
         } else {
-            Err(RespError::Other)
+            None
         }
     }
 
-    fn get_line_range_as_integer(
-        input: &[u8],
-        start_at: usize,
-    ) -> Result<(usize, usize, i64), RespError> {
-        let (start, end) = Self::get_line_range(input, start_at)?;
-        let s = std::str::from_utf8(&input[start..end - 2]).map_err(|_| RespError::Other)?;
-        let integer = s.parse().map_err(|_| RespError::Other)?;
-        Ok((start, end, integer))
+    pub fn as_string(&self) -> Option<&str> {
+        self.as_bytes()
+            .map(|x| std::str::from_utf8(x).ok())
+            .flatten()
     }
-}
 
-#[derive(Debug)]
-pub struct RespTypeIter<'a> {
-    data: &'a [u8],
-    closed: bool,
-    scan_start: usize,
-    length: usize,
-}
+    pub fn as_error_string(&self) -> Option<&str> {
+        self.as_error_bytes()
+            .map(|x| std::str::from_utf8(x).ok())
+            .flatten()
+    }
 
-impl<'a> RespTypeIter<'a> {
-    pub fn new(data: &'a [u8]) -> RespTypeIter<'a> {
-        RespTypeIter {
-            data,
-            closed: false,
-            scan_start: 0,
-            length: 1,
+    pub fn into_bytes(self) -> Option<Vec<u8>> {
+        use RespType::*;
+
+        match self {
+            SimpleString(data) => Some(data),
+            BulkString(data) => Some(data),
+            _ => None,
         }
     }
 
-    fn inner_loop(&mut self) -> ResultXp<'a> {
-        let out = match self.data[self.scan_start] {
-            b'+' => {
-                let (start, end) = RespTypeScan::get_line_range(self.data, self.scan_start + 1)?;
-                self.scan_start = end + 1;
-                RespTypeScan::SimpleString(&self.data[start..end - 2])
-            }
-            b'-' => {
-                let (start, end) = RespTypeScan::get_line_range(self.data, self.scan_start + 1)?;
-                self.scan_start = end + 1;
-                RespTypeScan::Error(&self.data[start..end - 2])
-            }
-            b':' => {
-                let (_start, end, integer) =
-                    RespTypeScan::get_line_range_as_integer(self.data, self.scan_start + 1)?;
-                self.scan_start = end;
-                RespTypeScan::Integer(integer)
-            }
-            b'$' => {
-                let (_start, end, integer) =
-                    RespTypeScan::get_line_range_as_integer(self.data, self.scan_start + 1)?;
-                self.scan_start = end;
+    pub fn into_error_bytes(self) -> Option<Vec<u8>> {
+        if let RespType::Error(error) = self {
+            Some(error)
+        } else {
+            None
+        }
+    }
 
-                match integer {
-                    -1 => RespTypeScan::NullString,
-                    0 => {
-                        self.scan_start += 2;
-                        RespTypeScan::BulkString(b"")
-                    }
-                    x if x > 0 => {
-                        let start = self.scan_start;
-                        self.scan_start += x as usize;
-                        let data = &self.data[start..self.scan_start];
-                        self.scan_start += 2;
-                        RespTypeScan::BulkString(data)
-                    }
-                    _ => return Err(RespError::Other),
-                }
-            }
-            b'*' => {
-                let (_start, end, integer) =
-                    RespTypeScan::get_line_range_as_integer(self.data, self.scan_start + 1)?;
-                self.scan_start = end;
-                match integer {
-                    -1 => RespTypeScan::NullArray,
-                    0 => {
-                        self.length = 0;
-                        self.scan_start += 2;
-                        return Err(RespError::None);
-                    }
-                    x if x > 0 => {
-                        self.length = x as usize;
-                        RespTypeScan::ArrayStart(self.length)
-                    }
-                    _ => return Err(RespError::Other),
-                }
-            }
-            _ => return Err(RespError::Other),
+    pub fn into_string(self) -> Option<String> {
+        self.into_bytes()
+            .map(|x| String::from_utf8(x).ok())
+            .flatten()
+    }
+
+    pub fn into_error_string(self) -> Option<String> {
+        self.into_error_bytes()
+            .map(|x| String::from_utf8(x).ok())
+            .flatten()
+    }
+}
+
+impl Into<Result<Value, Value>> for RespType {
+    fn into(self) -> Result<Value, Value> {
+        use RespType::*;
+
+        if self.is_null() {
+            return Ok(Value::Null);
+        }
+
+        if self.as_error_string().is_some() {
+            return Err(Value::String(self.into_error_string().unwrap()));
         };
 
-        Ok(out)
-    }
-}
+        if self.as_error_bytes().is_some() {
+            return Err(Value::Bytes(self.into_error_bytes().unwrap()));
+        };
 
-impl<'a> Iterator for RespTypeIter<'a> {
-    type Item = ResultXp<'a>;
+        if self.as_string().is_some() {
+            return Ok(Value::String(self.into_string().unwrap()));
+        };
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.closed {
-            return None;
+        if self.as_bytes().is_some() {
+            return Ok(Value::Bytes(self.into_bytes().unwrap()));
+        };
+
+        match self {
+            Integer(data) => Ok(Value::Int(data)),
+            Array(data) => {
+                let converted: Result<Vec<Value>, Value> =
+                    data.into_iter().map(|x| x.into()).collect();
+                Ok(Value::Array(converted?))
+            }
+            _ => unreachable!(),
         }
-
-        if self.data.get(self.scan_start).is_none() {
-            return None;
-        }
-
-        if self.length == 0 {
-            return None;
-        }
-
-        let out = self.inner_loop();
-        if let Ok(RespTypeScan::ArrayStart(_)) = out {
-            ()
-        } else {
-            self.length = self.length.saturating_sub(1);
-        }
-
-        if Err(RespError::None) == out {
-            return None;
-        }
-
-        Some(out)
     }
 }
 
 #[test]
-fn parse_simple_string_test() {
+fn resp_type_into_value_error_string() {
+    let result: Result<Value, Value> = RespType::Error(b"error".to_vec()).into();
+
+    assert_eq!(result, Err(Value::String("error".to_string())))
+}
+
+#[test]
+fn resp_type_into_value_error_bytes() {
+    let result: Result<Value, Value> = RespType::Error(b"\xfe\xfe\xff\xff".to_vec()).into();
+    assert_eq!(result, Err(Value::Bytes([254, 254, 255, 255].to_vec())))
+}
+
+#[test]
+fn resp_type_into_value_bulkstring_string() {
+    let result: Result<Value, Value> = RespType::BulkString(b"text".to_vec()).into();
+
+    assert_eq!(result, Ok(Value::String("text".to_string())))
+}
+
+#[test]
+fn resp_type_into_value_bulkstring_bytes() {
+    let result: Result<Value, Value> =
+        RespType::BulkString([254, 254, 255, 255, 1, 2, 3, 4].to_vec()).into();
+
     assert_eq!(
+        result,
+        Ok(Value::Bytes([254, 254, 255, 255, 1, 2, 3, 4].to_vec()))
+    )
+}
+
+#[test]
+fn resp_type_into_value_null() {
+    let result: Result<Value, Value> = RespType::NullString.into();
+
+    assert_eq!(result, Ok(Value::Null));
+
+    let result: Result<Value, Value> = RespType::NullArray.into();
+
+    assert_eq!(result, Ok(Value::Null));
+}
+
+#[test]
+fn resp_type_into_value_array() {
+    let result: Result<Value, Value> = RespType::Array(vec![
         RespType::SimpleString(b"OK".to_vec()),
-        RespType::parse(b"+OK\r\n").unwrap()
-    );
-    assert!(RespType::parse(b"+OK\r").is_err());
+        RespType::BulkString(b"just some text".to_vec()),
+        RespType::NullString,
+        RespType::Array(vec![]),
+    ])
+    .into();
+
+    assert_eq!(
+        result,
+        Ok(Value::Array(vec![
+            Value::String("OK".to_string()),
+            Value::String("just some text".to_string()),
+            Value::Null,
+            Value::Array(vec![])
+        ]))
+    )
 }
 
 #[test]
-fn iter_test_1() {
-    let mut a = RespTypeIter::new(b"+OK\r\n");
-    let item = a.next().unwrap().unwrap();
+fn convert_text_to_value() {
+    let mut parser = Parser::new_from_bytes(b"$14\r\njust some text\r\n");
+    let item = parser.parse().unwrap().to_owned();
+    let result: Result<Value, Value> = item.into();
 
-    assert_eq!(RespTypeScan::SimpleString(b"OK"), item);
-    assert_eq!(None, a.next());
-}
-
-#[test]
-fn iter_test_2() {
-    let mut a = RespTypeIter::new(b"-ERROR\r\n");
-    let item = a.next().unwrap().unwrap();
-
-    assert_eq!(RespTypeScan::Error(b"ERROR"), item);
-    assert_eq!(None, a.next());
-}
-
-#[test]
-fn iter_test_3() {
-    let mut a = RespTypeIter::new(b":1234\r\n");
-    let item = a.next().unwrap().unwrap();
-
-    assert_eq!(RespTypeScan::Integer(1234), item);
-    assert_eq!(None, a.next());
-}
-
-#[test]
-fn iter_test_4() {
-    let mut a = RespTypeIter::new(b"$5\r\nhello\r\n");
-    let item = a.next().unwrap().unwrap();
-
-    assert_eq!(RespTypeScan::BulkString(b"hello"), item);
-    assert_eq!(None, a.next());
-}
-
-#[test]
-fn iter_test_5() {
-    let mut a = RespTypeIter::new(b"$0\r\n\r\n");
-    let item = a.next().unwrap().unwrap();
-
-    assert_eq!(RespTypeScan::BulkString(b""), item);
-    assert_eq!(None, a.next());
-}
-
-#[test]
-fn iter_test_6() {
-    let mut a = RespTypeIter::new(b"$-1\r\n");
-    let item = a.next().unwrap().unwrap();
-
-    assert_eq!(RespTypeScan::NullString, item);
-    assert_eq!(None, a.next());
-}
-
-#[test]
-fn iter_test_7() {
-    let mut a = RespTypeIter::new(b"*3\r\n:1\r\n:2\r\n:3\r\n");
-    let item = a.next().unwrap().unwrap();
-    assert_eq!(RespTypeScan::ArrayStart(3), item);
-    let item = a.next().unwrap().unwrap();
-    assert_eq!(RespTypeScan::Integer(1), item);
-    let item = a.next().unwrap().unwrap();
-    assert_eq!(RespTypeScan::Integer(2), item);
-    let item = a.next().unwrap().unwrap();
-    assert_eq!(RespTypeScan::Integer(3), item);
-    assert_eq!(None, a.next());
-}
-
-#[test]
-fn iter_test_8() {
-    let mut a = RespTypeIter::new(b"*0\r\n");
-    assert_eq!(None, a.next());
-}
-
-#[test]
-fn iter_test_9() {
-    let mut a = RespTypeIter::new(b"*-1\r\n");
-    let item = a.next().unwrap().unwrap();
-    assert_eq!(RespTypeScan::NullArray, item);
+    assert_eq!(result, Ok(Value::String("just some text".to_string())))
 }
